ca4173d416187c77d51b525100a3db71773cbf43
kingluo <home_king@163.com>
2024-12-22 22:39:34 +0800

    fix header comment

--- coco.h
+++ coco.h
@@ -2,7 +2,7 @@
 
 // MIT License
 //
-// Copyright (c) 2024 luajit.io@gmail.com
+// Copyright (c) 2024 jinhua luo, luajit.io@gmail.com
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
